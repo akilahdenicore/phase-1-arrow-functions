72a4d6611303a58d940f748a1a236f43812cdbce
Akilah Snowden <akilahdsnowden@gmail.com>
2022-04-26 21:39:43 -0400

    commit tree d17b75e7eae9ce56ea05648aabb9fe0fd99b0d04

---
+++ index.js
@@ -0,0 +1,18 @@
+//has a function expression called divide
+//divides 2000/100
+const divide = function(){ //function to the right of the assignment operator is called a function expression
+    return 2000 / 100;
+}
+
+
+/*arrow function called square
+ takes one parameter and multiplies it times itself
+ let x = 2
+ expect
+ */
+ const square = (number) => Math.pow(number, 2);
+ //Math.power()accepts two parameters and returns the value of first parameter raised to the second parameter.
+
+ //add arrow function takes two parameters and adds them together
+
+ const add = (num1, num2) => num1 + num2;
